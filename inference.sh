0009ace8c1712222d4f62e6f2459c28cfb7ad4e4
ZhaoWorkStation <dungbuit1k28@gmail.com>
2025-03-01 03:43:30 +0700

    Initial commit

---
+++ inference.sh
@@ -0,0 +1,13 @@
+python main.py \
+    --eval \
+    --weighted_keywords \
+    --enable_generation \
+    --inference_type unixcoder_with_rl \
+    --generator_model_path deepseek-ai/deepseek-coder-6.7b-base \
+    --retriever_model_path 'nov3630/RLRetriever' \
+    --generator_max_crossfile_length 1536 \
+    --generator_max_context_length 2048 \
+    --generator_batch_size_per_gpu 4 \
+    ----retriever_batch_size_per_gpu 16 \
+    --output_dir result_infer/RLCoder_deepseekcoder_7b_crossfile_1536_infile_512 \
+    2>&1|tee log_infer/RLCoder_deepseekcoder_7b_crossfile_1536_infile_512.log
